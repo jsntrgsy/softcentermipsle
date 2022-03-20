dfde8aab24a34e14437eedfad42ccbe7d45ffd54
Xu Zhaowei <paldier@hotmail.com>
2022-03-20 15:59:05 +0800

    add ddns

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -92,6 +92,17 @@
             "version": "1.0.2"
         }, 
         {
+            "arch": "mipsle", 
+            "build_date": "2022-03-20_15:59:01", 
+            "description": "支持Alidns(阿里云) Dnspod(腾讯云) Cloudflare 华为云 Callback", 
+            "home_url": "Module_ddns.asp", 
+            "md5": "210644105a02c5705407e6da856a0b2c", 
+            "name": "ddns", 
+            "tar_url": "ddns/ddns.tar.gz", 
+            "title": "多ddns合一", 
+            "version": "0.1"
+        }, 
+        {
             "build_date": "2020-08-20_11:42:34", 
             "description": "内网穿透利器，谁用谁知道。", 
             "home_url": "Module_frpc.asp", 
